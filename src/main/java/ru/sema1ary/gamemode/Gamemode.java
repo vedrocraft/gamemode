2099244a9a6823c897ca9c99cb0cc6268f6b824b
LittleVidoskim <VIDOSKIM@yandex.ru>
2025-03-07 00:45:44 +0300

    init

---
+++ src/main/java/ru/sema1ary/gamemode/Gamemode.java
@@ -0,0 +1,24 @@
+package ru.sema1ary.gamemode;
+
+import org.bukkit.plugin.java.JavaPlugin;
+import ru.sema1ary.gamemode.command.GamemodeCommand;
+import ru.vidoskim.bukkit.service.ConfigService;
+import ru.vidoskim.bukkit.service.impl.ConfigServiceImpl;
+import ru.vidoskim.bukkit.util.LiteCommandUtil;
+import service.ServiceManager;
+
+public final class Gamemode extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        ServiceManager.registerService(ConfigService.class, new ConfigServiceImpl(this));
+
+        new LiteCommandUtil().create("gamemode",
+                new GamemodeCommand(ServiceManager.getService(ConfigService.class)));
+    }
+
+    @Override
+    public void onDisable() {
+        ServiceManager.disableServices();
+    }
+}
---
+++ src/main/java/ru/sema1ary/gamemode/command/GamemodeCommand.java
@@ -0,0 +1,83 @@
+package ru.sema1ary.gamemode.command;
+
+import dev.rollczi.litecommands.annotations.argument.Arg;
+import dev.rollczi.litecommands.annotations.async.Async;
+import dev.rollczi.litecommands.annotations.command.Command;
+import dev.rollczi.litecommands.annotations.context.Context;
+import dev.rollczi.litecommands.annotations.execute.Execute;
+import dev.rollczi.litecommands.annotations.permission.Permission;
+import net.kyori.adventure.text.minimessage.MiniMessage;
+import org.bukkit.GameMode;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import ru.vidoskim.bukkit.service.ConfigService;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Command(name = "gamemode", aliases = {"gm"})
+public class GamemodeCommand {
+    private final MiniMessage miniMessage = MiniMessage.miniMessage();
+    private final ConfigService configService;
+    private final Map<Integer, GameMode> gamemodeMap = new HashMap<>();
+
+    public GamemodeCommand(ConfigService configService) {
+        this.configService = configService;
+        gamemodeMap.put(0, GameMode.SURVIVAL);
+        gamemodeMap.put(1, GameMode.CREATIVE);
+        gamemodeMap.put(2, GameMode.SPECTATOR);
+        gamemodeMap.put(3, GameMode.ADVENTURE);
+    }
+
+    @Async
+    @Execute(name = "reload")
+    @Permission("gamemode.reload")
+    void reload(@Context CommandSender sender) {
+        configService.reload();
+        sender.sendMessage(miniMessage.deserialize(configService.get("reload-message")));
+    }
+
+    @Async
+    @Execute
+    void execute(@Context Player sender, @Arg("режим") GameMode gameMode) {
+        sender.setGameMode(gameMode);
+        sender.sendMessage(miniMessage.deserialize(
+               ((String) configService.get("change-message")).replace("{gamemode}", gameMode.toString())
+        ));
+    }
+
+    @Async
+    @Execute
+    void execute(@Context Player sender, @Arg("режим") int id) {
+        GameMode gameMode = gamemodeMap.get(id);
+        sender.setGameMode(gameMode);
+        sender.sendMessage(miniMessage.deserialize(
+                ((String) configService.get("change-message")).replace("{gamemode}", gameMode.toString())
+        ));
+    }
+
+    @Async
+    @Execute
+    @Permission("gamemode.other")
+    void execute(@Context CommandSender sender, @Arg("игрок") Player target
+            , @Arg("режим") GameMode gameMode) {
+        target.setGameMode(gameMode);
+        sender.sendMessage(miniMessage.deserialize(
+                ((String) configService.get("target-change-message")).replace("{gamemode}", gameMode.toString())
+                        .replace("{player}", target.getName())
+        ));
+    }
+
+    @Async
+    @Execute
+    @Permission("gamemode.other")
+    void execute(@Context Player sender, @Arg("игрок") Player target
+            , @Arg("режим") int id) {
+        GameMode gameMode = gamemodeMap.get(id);
+        target.setGameMode(gameMode);
+        sender.sendMessage(miniMessage.deserialize(
+                ((String) configService.get("target-change-message")).replace("{gamemode}", gameMode.toString())
+                        .replace("{player}", target.getName())
+        ));
+    }
+}
